063cb2c21a608ca5625db275fb6f7559ab787093
Astha Samaddar <prachisharmakv@gmail.com>
2023-04-10 15:35:43 +0530

    p

--- art_gallery/index.js
+++ art_gallery/index.js
@@ -1,14 +1,16 @@
-let myIndex = 0;
-carousel();
+// let myIndex = 0;
+// carousel();
 
-function carousel() {
-  let i;
-  let x = document.getElementsByClassName("gal-img");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
-}
+// function carousel() {
+//   let i;
+//   let x = document.getElementsByClassName("gal-img");
+//   for (i = 0; i < x.length; i++) {
+//     x[i+2].style.display = "none";  
+    
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {myIndex = 1}    
+//   x[myIndex-1].style.display = "block";  
+//   setTimeout(carousel, 2000); // Change image every 2 seconds
+// }
+
