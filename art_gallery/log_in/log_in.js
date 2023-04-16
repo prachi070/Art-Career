185d9f84d0725a92ab36583cb227dce3b8b12ba9
Astha Samaddar <prachisharmakv@gmail.com>
2023-04-17 00:48:47 +0530

    prachi

---
+++ art_gallery/log_in/log_in.js
@@ -0,0 +1,27 @@
+
+
+function validateform() {
+    var name = document.myForm.fname.value;
+    var passwordlength = document.myForm.fpass.value;
+    var email=document.myForm.femail.value;
+    var phone=document.myForm.fphone.value;
+    var status = false;
+    if (name.length == "") {
+        document.getElementById("formerror1").innerHTML =" *Please enter your name";
+        status = false;
+    } else {
+        document.getElementById("formerror1").innerHTML = " ";
+        status = true;
+    }
+
+
+
+    if (passwordlength.length < 6) {
+        document.getElementById("formerror4").innerHTML =" *Password must be greater than 6";
+        status = false;
+    } else {
+        document.getElementById("formerror4").innerHTML = " ";
+    }
+
+    return status;
+}
